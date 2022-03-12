02f4a55eac5e45a4782757f40eeba6ea077a2e86
aylin <aylin-haykir@web.de>
2022-03-12 18:36:40 +0100

    Initial commit

---
+++ project/Platformer/scr/Actors/Actor.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+class_name Actor
+
+
+const FLOOR_NORMAL = Vector2.UP
+
+export var speed: = Vector2(400.0, 800.0)
+export var gravity: = 1440.0
+export var max_y_velocity: = 3000
+
+var velocity: = Vector2.ZERO
